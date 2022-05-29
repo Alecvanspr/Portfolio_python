824af06cd4092c6580c6ff272dc41998ee958e85
Alecvanspr <Alecvanspr@gmail.com>
2022-05-29 21:20:15 +0200

    Database geïnitialiseerd, er is een card visable, klaar voor de uitbreidingen

--- webshop/webshop/polls/views.py
+++ webshop/webshop/polls/views.py
@@ -2,13 +2,18 @@ import re
 from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.http import HttpResponse
+from .models import Product
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 def homeScreen(request):
+    first_item = Product.objects.filter()
     return render(
         request,
-        'product/Producten.html'
+        'product/Producten.html',
+        {
+            "producten": first_item,
+        }
     )
 def addProduct(request):
     return render(
